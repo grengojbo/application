c1f0415077d12c3e35638f3978fc81293754f4af
Oleg Dolya <oleg.dolya@gmail.com>
2012-10-02 13:10:33 +0300

    modify metadata

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "1.0.0"
 recipe           "application", "Loads application databags and selects recipes to use"
 
-%w{ search }.each do |cb|
+%w{ python gunicorn }.each do |cb|
   depends cb
 end
 
